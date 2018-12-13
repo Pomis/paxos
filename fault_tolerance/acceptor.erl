63dbdccee9c94f6c38a2e3497975eb385b5a9ebe
Carlos Rojas Morales <crojasmrls@gmail.com>
2018-12-13 22:51:10 +0100

    fault tolerance

--- fault_tolerance/acceptor.erl
+++ fault_tolerance/acceptor.erl
@@ -2,43 +2,54 @@
 -module(acceptor).
 -export([start/2]).
 -define(delay, 200).
--define(drop, 1).
+-define(drop, 3).
 
 start(Name, PanelId) ->
   spawn(fun() -> init(Name, PanelId) end).
         
-init(Name, PanelId) ->
-  Promised = order:null(), 
-  Voted = order:null(),
-  Value = na,
-  acceptor(Name, Promised, Voted, Value, PanelId).
-
+  init(Name, PanelId) ->
+  pers:open(Name),
+  {Promised,Voted,Value,PanelIdR}=pers:read(Name),
+  pers:close(Name),
+  if PanelIdR==na ->
+      acceptor(Name, Promised, Voted, Value, PanelId);
+    true ->
+      acceptor(Name, Promised, Voted, Value, PanelIdR)
+  end.
+  
 acceptor(Name, Promised, Voted, Value, PanelId) ->
+    R11 = rand:uniform(?delay),
+    timer:sleep(R11),
   receive
     {prepare, Proposer, Round} ->
       R1 = rand:uniform(?delay),
       timer:sleep(R1),
       P1 = rand:uniform(10),
       if P1 =< ?drop ->
-          io:format("message from proposer dropped~n"),
+          io:format("promise message from proposer dropped~n"),
           acceptor(Name, Promised, Voted, Value, PanelId);
         true ->
           case order:gr(Round, Promised) of
             true ->
             P2 = rand:uniform(10),
               
+              pers:open(Name),
+              pers:store(Name,Round,Voted,Value,PanelId),
+              pers:close(Name),              
               io:format("[Acceptor ~w] Phase 1: promised ~w voted ~w colour ~w~n",
                      [Name, Round, Voted, Value]),
               % Update gui
               Colour = case Value of na -> {0,0,0}; _ -> Value end,
               PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Voted]), 
                          "Promised: " ++ io_lib:format("~p", [Round]), Colour},
-              %if P2 =< ?drop ->
-              %io:format("message from acceptor dropped~n");
-              %  true ->
+              if P2 =< ?drop ->
+                io:format("promise message from acceptor dropped~n"),
+               % paxy:crash(Name),
+                 acceptor(Name, Round, Voted, Value, PanelId);
+                true ->
                   Proposer ! {promise, Round, Voted, Value}, 
-              %end;                          
-              acceptor(Name, Round, Voted, Value, PanelId);
+                  acceptor(Name, Round, Voted, Value, PanelId)
+              end;                                        
             false ->
              % Proposer ! {sorry, {prepare, Round}},
               acceptor(Name, Promised, Voted, Value, PanelId)
@@ -47,23 +58,37 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
     {accept, Proposer, Round, Proposal} ->
       R2 = rand:uniform(?delay),
       timer:sleep(R2),
-      case order:goe(Round, Promised) of
-        true ->
-          Proposer ! {vote, Round},
-          case order:goe(Round, Voted) of
+      P3 = rand:uniform(10),
+      if P3 =< ?drop ->
+          io:format("accept message from proposer dropped~n"),
+          acceptor(Name, Promised, Voted, Value, PanelId);
+      true ->
+        case order:goe(Round, Promised) of
+          true ->
+            P4 = rand:uniform(10),
+            if P4 =< ?drop ->
+              io:format("accept message from acceptor dropped~n");
             true ->
-      io:format("[Acceptor ~w] Phase 2: promised ~w voted ~w colour ~w~n",
-                 [Name, Promised, Round, Proposal]),
-              % Update gui
-              PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Round]), 
-                         "Promised: " ++ io_lib:format("~p", [Promised]), Proposal},
-              acceptor(Name, Promised, Round, Proposal, PanelId);
-            false ->
-              acceptor(Name, Promised, Voted, Value, PanelId)
-          end;                            
-        false ->
-          %Proposer ! {sorry, {accept, Round}},
-          acceptor(Name, Promised, Voted, Value, PanelId)
+              Proposer ! {vote, Round}
+            end,
+            case order:goe(Round, Voted) of
+              true ->
+              pers:open(Name),
+              pers:store(Name,Promised,Round,Proposal,PanelId),
+              pers:close(Name),   
+        io:format("[Acceptor ~w] Phase 2: promised ~w voted ~w colour ~w~n",
+                   [Name, Promised, Round, Proposal]),
+                % Update gui
+                PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Round]), 
+                           "Promised: " ++ io_lib:format("~p", [Promised]), Proposal},
+                acceptor(Name, Promised, Round, Proposal, PanelId);
+              false ->
+                acceptor(Name, Promised, Voted, Value, PanelId)
+            end;                            
+          false ->
+            %Proposer ! {sorry, {accept, Round}},
+            acceptor(Name, Promised, Voted, Value, PanelId)
+        end
       end;
     stop ->
       PanelId ! stop,
--- fault_tolerance/paxy.erl
+++ fault_tolerance/paxy.erl
@@ -1,63 +1,87 @@
 -module(paxy).
--export([start/1, stop/0, stop/1]).
+-export([start_proposers/2, start_acceptors/0, stop/0, stop/1, crash/1]). 
 
--define(RED, {255,0,0}).
+-define(RED, {255,0,0}). 
 -define(BLUE, {0,0,255}).
 -define(GREEN, {0,255,0}).
 
-% Sleep is a list with the initial sleep time for each proposer
-start(Sleep) ->
-  AcceptorNames = ["Acceptor a", "Acceptor b", "Acceptor c", "Acceptor d", 
-                   "Acceptor e"],
-  AccRegister = [a, b, c, d, e],
-  ProposerNames = [{"Proposer kurtz", ?RED}, {"Proposer kilgore", ?GREEN}, 
-                   {"Proposer willard", ?BLUE}],
-  PropInfo = [{kurtz, ?RED}, {kilgore, ?GREEN}, {willard, ?BLUE}],
-  register(gui, spawn(fun() -> gui:start(AcceptorNames, ProposerNames) end)),
-  gui ! {reqState, self()},
-  receive
-    {reqState, State} ->
-      {AccIds, PropIds} = State,
-      start_acceptors(AccIds, AccRegister),
-      start_proposers(PropIds, PropInfo, AccRegister, Sleep)
-  end,
-  true.
-    
+
+% Entry point of the application. Sleep is a list with the initial sleep time for each proposer
+start_proposers(Sleep, AcceptorsNode) ->
+    %Node='paxy-acceptors@cod009',
+    AccRegister = [{a, AcceptorsNode}, {b, AcceptorsNode},{c, AcceptorsNode},{d, AcceptorsNode},{e, AcceptorsNode}],
+    PropInfo = [{kurtz, ?RED}, {kilgore, ?GREEN}, {willard, ?BLUE}],
+    {gui, AcceptorsNode} ! {reqState, self()}, 
+    receive
+        {reqState, State} ->
+            {_, PropIds} = State,
+            start_proposers(PropIds, PropInfo, AccRegister, Sleep)
+    end,
+    true.
+
+start_acceptors() ->
+    AcceptorNames = ["Acceptor a", "Acceptor b", "Acceptor c", "Acceptor d", "Acceptor e"],
+    AccRegister = [a, b, c, d, e],
+    ProposerNames = [{"Proposer kurtz", ?RED}, {"Proposer kilgore", ?GREEN}, {"Proposer willard", ?BLUE}],
+    register(gui, spawn(fun() -> gui:start(AcceptorNames, ProposerNames) end)), % create new gui process
+    gui ! {reqState, self()}, % send reference of self (own PID) to GUI
+    receive
+        {reqState, State} ->
+            {AccIds, _} = State,
+            start_acceptors(AccIds, AccRegister)
+    end,
+    true.
+
+
 start_acceptors(AccIds, AccReg) ->
-  case AccIds of
-    [] ->
-      ok;
-    [AccId|Rest] ->
-      [RegName|RegNameRest] = AccReg,
-      register(RegName, acceptor:start(RegName, AccId)),
-      start_acceptors(Rest, RegNameRest)
-  end.
+    case AccIds of
+        [] ->
+            ok;
+        [AccId|Rest] ->
+            [RegName|RegNameRest] = AccReg,
+            register(RegName, acceptor:start(RegName, AccId)),
+            start_acceptors(Rest, RegNameRest)
+    end.
 
 start_proposers(PropIds, PropInfo, Acceptors, Sleep) ->
-  case PropIds of
-    [] ->
-      ok;
-    [PropId|Rest] ->
-      [{RegName, Colour}|RestInfo] = PropInfo,
-      [FirstSleep|RestSleep] = Sleep,
-      proposer:start(RegName, Colour, Acceptors, FirstSleep, PropId),	
-      start_proposers(Rest, RestInfo, Acceptors, RestSleep)
-  end.
+    case PropIds of
+        [] ->
+            ok;
+        [PropId|Rest] ->
+            [{RegName, Colour}|RestInfo] = PropInfo,
+            [FirstSleep|RestSleep] = Sleep,
+            proposer:start(RegName, Colour, Acceptors, FirstSleep, PropId), 
+            start_proposers(Rest, RestInfo, Acceptors, RestSleep)
+        end.
 
 stop() ->
-  stop(a),
-  stop(b),
-  stop(c),
-  stop(d),
-  stop(e),
-  stop(gui).
+    stop(a),
+    stop(b),
+    stop(c),
+    stop(d),
+    stop(e),
+    stop(gui).
 
 stop(Name) ->
-  case whereis(Name) of
-    undefined ->
-      ok;
-    Pid ->
-      Pid ! stop
-  end.
+    case whereis(Name) of
+        undefined ->
+            ok;
+        Pid ->
+            pers:delete(Name),
+            Pid ! stop
+    end.
 
- 
+crash(Name) ->
+    case whereis(Name) of
+        undefined ->
+            ok;
+        Pid ->
+            pers:open(Name),
+            {_, _, _, Pn} = pers:read(Name),
+            Pn ! {updateAcc, "Voted: CRASHED", "Promised: CRASHED", {0,0,0}},
+            dets:close(Name),
+            unregister(Name),
+            exit(Pid, "crash"),
+            timer:sleep(2000),
+            register(Name, acceptor:start(Name, na))
+    end.
